c952ca561154430899c1069eff2d635adc82ecc2
Andras Tarsoly <tarsolya@secretsaucepartners.com>
2013-11-07 23:12:10 +0100

    Dry up loading configuration for MongoMapper

--- admin.rb
+++ admin.rb
@@ -8,6 +8,7 @@ require 'sinatra'
 require 'sinatra/config_file'
 require 'active_support/json'
 require 'app/abba'
+require 'helpers'
 
 require 'newrelic_rpm'
 
@@ -16,16 +17,9 @@ config_file 'config.yml'
 configure do
   ActiveSupport.escape_html_entities_in_json = true
 
-  if File.file?(settings.mongo_config_file)
-    config = YAML.load(ERB.new(File.read(settings.mongo_config_file)).result)
-    MongoMapper.setup(config, settings.environment.to_s)
-  else
-    MongoMapper.setup({
-      'production'  => {'uri' => ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI']},
-      'staging'  => {'uri' => ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI']},
-      'development' => {'uri' => 'mongodb://localhost:27017/abba'}
-    }, settings.environment.to_s)
-  end
+  # Configure MongoMapper
+  mongo_config = Helpers::Configuration.mongomapper_config(settings.mongo_config_file)
+  MongoMapper.setup(mongo_config, settings.environment.to_s)
 
   env = Sprockets::Environment.new(settings.root)
 
